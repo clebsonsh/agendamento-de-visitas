996f4c44e53ea75c11220f6f4e6966c93347ae3f
Clebson Moura <clebson.sh@gmail.com>
2025-11-08 20:12:14 -0300

    feat: busca as agendas juntamente com o veículo.

--- api/src/Controllers/VehicleController.php
+++ api/src/Controllers/VehicleController.php
@@ -4,21 +4,25 @@ declare(strict_types=1);
 
 namespace Api\Controllers;
 
+use Api\Repositories\ScheduleRepository;
 use Api\Repositories\VehicleRepository;
+use Api\Services\ScheduleService;
 use Api\Services\VehicleService;
 
 class VehicleController
 {
     private VehicleService $vehicleService;
 
+    private ScheduleService $scheduleService;
+
     public function __construct()
     {
         $this->vehicleService = new VehicleService(new VehicleRepository);
+        $this->scheduleService = new ScheduleService(new ScheduleRepository);
     }
 
     public function index()
     {
-
         response()->json([
             'vehicles' => $this->vehicleService->getAll(),
         ]);
@@ -34,8 +38,11 @@ class VehicleController
             ]);
         }
 
+        $schedules = $this->scheduleService->getByVehicleId($vehicle->id);
+
         response()->json([
             'vehicle' => $vehicle,
+            'schedules' => $schedules,
         ]);
     }
 }
